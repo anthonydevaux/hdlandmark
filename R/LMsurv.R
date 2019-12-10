f19445dd231d12b0e83b65267761f401a9814ef3
anthonydevaux <38243147+anthonydevaux@users.noreply.github.com>
2019-12-10 15:25:50 +0100

    updating

--- R/LMsurv.R
+++ R/LMsurv.R
@@ -12,12 +12,12 @@
 #'
 #' @examples
 #'
-LMsurv <- function(method = c("LM-cox","LM-rsf","cox","LM-cox-lasso"), models,
+LMsurv <- function(method = c("LM-cox","LM-rsf","cox","LM-coxnet","LM-splsDR"), models,
                    newdata, time, subject,
                    tHor){
 
-  if (!all(method%in%c("LM-cox","LM-rsf","cox","LM-cox-lasso"))){
-    stop("Only options available for method are 'LM-cox', 'LM-rsf', 'LM-cox-lasso' and 'cox'")
+  if (!all(method%in%c("LM-cox","LM-rsf","cox","LM-coxnet","LM-splsDR"))){
+    stop("Only options available for method are 'LM-cox', 'LM-rsf', 'LM-coxnet', 'LM-splsDR' and 'cox'")
   }
 
   nb_method <- length(method)
@@ -40,7 +40,7 @@ LMsurv <- function(method = c("LM-cox","LM-rsf","cox","LM-cox-lasso"), models,
 
     if (n > 1){
 
-      pred_surv[which(colnames(res_survfit$surv)%in%rownames(pred_surv)),"LM-cox"] <- res_survfit$surv[id_time,]
+      pred_surv[colnames(res_survfit$surv),"LM-cox"] <- res_survfit$surv[id_time,]
 
     }else{
 
@@ -63,7 +63,7 @@ LMsurv <- function(method = c("LM-cox","LM-rsf","cox","LM-cox-lasso"), models,
 
     if (n > 1){
 
-      pred_surv[which(colnames(res_survfit$surv)%in%rownames(pred_surv)),"cox"] <- res_survfit$surv[id_time,]
+      pred_surv[colnames(res_survfit$surv),"cox"] <- res_survfit$surv[id_time,]
 
     }else{
 
@@ -81,59 +81,114 @@ LMsurv <- function(method = c("LM-cox","LM-rsf","cox","LM-cox-lasso"), models,
     newdata.LMrsf <- subset(newdata, select = -c(serBilir2,serChol2,albumin,alkaline2,
                                                  SGOT2,platelets2,prothrombin2))
 
-      if (n > 1){
-
-        res_survfit <- randomForestSRC::predict.rfsrc(models[["LM-rsf"]], newdata.LMrsf)
-        id_time <- sum(res_survfit$time.interest <= tHor)
-        formula.xvar <- as.formula(as.character(model_obj$`LM-rsf`$call$formula)[c(1,3)])
-        id_no_na <- rownames(model.frame(formula.xvar,
-                                         newdata.LMrsf[,models[["LM-rsf"]]$xvar.names])) # id without NA
-        pred_surv[id_no_na,"LM-rsf"] <- res_survfit$survival[,id_time]
-
-      }else{
-
-        if (any(is.na(newdata.LMrsf[,models[["LM-rsf"]]$xvar.names]))){
-
-          pred_surv[1,"LM-rsf"] <- NA
-
-        }else{
-
-          res_survfit <- randomForestSRC::predict.rfsrc(models[["LM-rsf"]], newdata.LMrsf)
-          id_time <- sum(res_survfit$time.interest <= tHor)
-          pred_surv[1,"LM-rsf"] <- res_survfit$survival[id_time]
-
-        }
-
-      }
-
-  }
-
-
-  if (any(method == "LM-cox-lasso")){
-    if (is.null(models[["LM-cox-lasso"]])){
-      stop("No model found in models for method = LM-cox-lasso", "\n")
-    }
-
-    newdata.LMcoxLasso <- na.omit(subset(newdata, select = -c(id,year,serBilir2,serChol2,albumin,alkaline2,
-                                                 SGOT2,platelets2,prothrombin2)))
-
-    pred.LMcoxLasso <- predict(models[["LM-cox-lasso"]], data = newdata.LMcoxLasso)
-
-    res.surv.pred <- survival(pred.LMcoxLasso, time = tHor)
-
     if (n > 1){
 
-      pred_surv[intersect(names(res.surv.pred),rownames(pred_surv)),"LM-cox-lasso"] <-
-        res.surv.pred
+      res_survfit <- randomForestSRC::predict.rfsrc(models[["LM-rsf"]], newdata.LMrsf)
+      id_time <- sum(res_survfit$time.interest <= tHor)
+      formula.xvar <- as.formula(as.character(models[["LM-rsf"]]$call$formula)[c(1,3)])
+      id_no_na <- rownames(model.frame(formula.xvar,
+                                       newdata.LMrsf[,models[["LM-rsf"]]$xvar.names])) # id without NA
+      pred_surv[id_no_na,"LM-rsf"] <- res_survfit$survival[,id_time]
 
     }else{
 
-      pred_surv[1,"LM-cox-lasso"] <- res.surv.pred
+      if (any(is.na(newdata.LMrsf[,models[["LM-rsf"]]$xvar.names]))){
+
+        pred_surv[1,"LM-rsf"] <- NA
+
+      }else{
+
+        res_survfit <- randomForestSRC::predict.rfsrc(models[["LM-rsf"]], newdata.LMrsf)
+        id_time <- sum(res_survfit$time.interest <= tHor)
+        pred_surv[1,"LM-rsf"] <- res_survfit$survival[id_time]
+
+      }
 
     }
 
   }
 
+  if (any(method == "LM-coxnet")){
+    if (is.null(models[["LM-coxnet"]])){
+      stop("No model found in models for method = LM-coxnet", "\n")
+    }
+
+    newdata.LMcoxnet <- na.omit(subset(newdata, select = -c(id,year,serBilir2,serChol2,albumin,alkaline2,
+                                                            SGOT2,platelets2,prothrombin2)))
+
+    newdata.LMcoxnet <- as.data.frame(model.matrix( ~ .-1, newdata.LMcoxnet))
+
+    res_survfit <- survival::survfit(models[["LM-coxnet"]], newdata = newdata.LMcoxnet)
+    id_time <- sum(res_survfit$time <= tHor)
+
+    if (n > 1){
+
+      pred_surv[colnames(res_survfit$surv),"LM-coxnet"] <- res_survfit$surv[id_time,]
+
+    }else{
+
+      pred_surv[1,"LM-coxnet"] <- res_survfit$surv[id_time]
+
+    }
+  }
+
+  if (any(method == "LM-splsDR")){
+    if (is.null(models[["LM-splsDR"]])){
+      stop("No model found in models for method = LM-splsDR", "\n")
+    }
+
+    newdata.LMsplsDR <- na.omit(subset(newdata, select = -c(id,year,serBilir2,serChol2,albumin,alkaline2,
+                                                            SGOT2,platelets2,prothrombin2)))
+
+    newdata.LMsplsDR <- as.data.frame(model.matrix( ~ .-1, newdata.LMsplsDR))
+
+    Xnames <- rownames(models[["LM-splsDR"]]$splsDR_modplsr$loadings$X)
+
+    # centre reduit la matrice des nouveaux individus à partir du mean/sd du train
+    Xh.scale <- t((t(newdata.LMsplsDR[,Xnames])-models[["LM-splsDR"]]$XplanCent[Xnames])/models[["LM-splsDR"]]$XplanScal[Xnames])
+
+    X.spls <- matrix(NA, nrow = nrow(Xh.scale), ncol = ncol(models[["LM-splsDR"]]$tt_splsDR),
+                     dimnames = list(rownames(Xh.scale), colnames(models[["LM-splsDR"]]$tt_splsDR)))
+
+    u <- models[["LM-splsDR"]]$splsDR_modplsr$loadings$X
+
+    X.spls[,1] <- Xh.scale%*%u[,1]
+
+    if (ncol(X.spls) > 1){
+
+      for (h in 2:ncol(X.spls)){
+
+        th <- Xh.scale%*%u[,h-1]
+
+        proj.num <- th%*%t(th)
+        proj.den <- as.numeric(t(th)%*%th)
+        proj <- proj.num / proj.den
+
+        Xh.scale <- Xh.scale - proj%*%Xh.scale
+
+        Xh <- Xh.scale%*%u[,h]
+
+        X.spls[,h] <- Xh
+
+      }
+
+    }
+
+    # prediction de la matrice X test dans le nouvel espace par spls
+    res_survfit <- survival::survfit(models[["LM-splsDR"]]$cox_splsDR, newdata = as.data.frame(X.spls))
+    id_time <- sum(res_survfit$time <= tHor)
+
+    if (n > 1){
+
+      pred_surv[colnames(res_survfit$surv),"LM-splsDR"] <- res_survfit$surv[id_time,]
+
+    }else{
+
+      pred_surv[1,"LM-splsDR"] <- res_survfit$surv[id_time]
+
+    }
+
+  }
 
   return(pred_surv)
 }
